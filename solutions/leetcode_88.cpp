e094aafa00314f047d6e4b4a4c39ba0816a8ad97
Cpp Ikigai <cpp.ikigai@gmail.com>
2024-03-13 21:18:05 +0100

    Added m+n solution complexity for the problem 88. Merge Sorted Array.

--- solutions/leetcode_88.cpp
+++ solutions/leetcode_88.cpp
@@ -8,6 +8,32 @@
 
 void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n)
 {
+    int i = m - 1;
+    int j = n - 1;
+    int k = m + n - 1;
+    
+    // Continue merging until both arrays have elements
+    while (i >= 0 && j >= 0)
+    {
+        if (nums1[i] > nums2[j])
+        {
+            nums1[k--] = nums1[i--];
+        }
+        else
+        {
+            nums1[k--] = nums2[j--];
+        }
+    }
+
+    // If there are remaining elements in nums2, copy them to nums1
+    while (j >= 0)
+    {
+        nums1[k--] = nums2[j--];
+    }
+}
+/*
+void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n)
+{
     for(int i = 0; i < n; ++i)
     {
         nums1[m + i] = nums2[i];
@@ -15,7 +41,7 @@ void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n)
     
     sort(nums1.begin(), nums1.end());
 }
-
+*/
 void test(const std::string& name,
           const std::vector<int>& nums1, int m, 
           const std::vector<int>& nums2, int n,
